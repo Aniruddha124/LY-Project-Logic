7a799f0c5cc2631305bbf839ca5610550971f6dc
Shreyans <s.mulkutkar@somaiya.edu>
2023-02-12 20:53:43 +0530

    updated Format.py and Predict.py

--- Algorithm/Format.py
+++ Algorithm/Format.py
@@ -8,18 +8,18 @@ import json
 # print(data)
 
 def format_transaction_details(json):
-    df = pd.DataFrame(columns=["tx_hash", "in_degree", "out_degree", "in_btc", "out_btc", "total_btc", "mean_in_btc", "mean_out_btc"])
-    print(df)
+    df = pd.DataFrame(columns=["tx_hash", "in_degree", "out_degree", "in_btc", "out_btc", "total_btc", "mean_in_btc", "mean_out_btc", "is_malicious"])
+    # print(df)
     in_degree = len(json['bitcoin']['inputs'])
     out_degree = len(json['bitcoin']['outputs'])
-    print(in_degree)
-    print(out_degree)
+    # print(in_degree)
+    # print(out_degree)
     for i in range(in_degree):
-        new_row = {'tx_hash': json['bitcoin']['inputs'][i]['transaction']['hash'], 'in_degree': in_degree, 'out_degree': out_degree, 'in_btc': json['bitcoin']['inputs'][i]['value'], 'out_btc': 0, 'total_btc': json['bitcoin']['inputs'][i]['value'], 'mean_in_btc': json['bitcoin']['inputs'][i]['value']/in_degree, 'mean_out_btc': 0}
+        new_row = {'tx_hash': json['bitcoin']['inputs'][i]['transaction']['hash'], 'in_degree': in_degree, 'out_degree': out_degree, 'in_btc': json['bitcoin']['inputs'][i]['value'], 'out_btc': 0, 'total_btc': json['bitcoin']['inputs'][i]['value'], 'mean_in_btc': json['bitcoin']['inputs'][i]['value']/in_degree, 'mean_out_btc': 0, 'is_malicious': 0}
         df = df.append(new_row, ignore_index=True)
     
     for i in range(out_degree):
-        new_row = {'tx_hash': json['bitcoin']['outputs'][i]['transaction']['hash'], 'in_degree': in_degree, 'out_degree': out_degree, 'in_btc': 0, 'out_btc': json['bitcoin']['outputs'][i]['value'], 'total_btc': json['bitcoin']['outputs'][i]['value'], 'mean_in_btc': 0, 'mean_out_btc': json['bitcoin']['outputs'][i]['value']/out_degree}
+        new_row = {'tx_hash': json['bitcoin']['outputs'][i]['transaction']['hash'], 'in_degree': in_degree, 'out_degree': out_degree, 'in_btc': 0, 'out_btc': json['bitcoin']['outputs'][i]['value'], 'total_btc': json['bitcoin']['outputs'][i]['value'], 'mean_in_btc': 0, 'mean_out_btc': json['bitcoin']['outputs'][i]['value']/out_degree, 'is_malicious': 0}
         df = df.append(new_row, ignore_index=True)
     
     return(df)
--- Algorithm/Predict.py
+++ Algorithm/Predict.py
@@ -11,17 +11,21 @@ def predict(address):
     tx_data = get_transaction_details(address)
     # print(tx_data['data'])
     df = format_transaction_details(tx_data['data'])
-    # print(df)
+    # print(len(df))
     for i in range(len(df)):
-        in_degree = df.iloc[i]["in_degree"]
-        out_degree = df.iloc[i]["out_degree"]
-        in_btc = df.iloc[i]["in_btc"]
-        out_btc = df.iloc[i]["out_btc"]
-        total_btc = df.iloc[i]["total_btc"]
-        mean_in_btc = df.iloc[i]["mean_in_btc"]
-        mean_out_btc = df.iloc[i]["mean_out_btc"]
+        in_degree = df["in_degree"].iloc[i]
+        out_degree = df["out_degree"].iloc[i]
+        in_btc = df["in_btc"].iloc[i]
+        out_btc = df["out_btc"].iloc[i]
+        total_btc = df["total_btc"].iloc[i]
+        mean_in_btc = df["mean_in_btc"].iloc[i]
+        mean_out_btc = df["mean_out_btc"].iloc[i]
+        # print(in_degree, out_degree, in_btc, out_btc, total_btc, mean_in_btc, mean_out_btc)
         is_malicious = model.predict([[in_degree, out_degree, in_btc, out_btc, total_btc, mean_in_btc, mean_out_btc]])
-        print(is_malicious)
+        # print(is_malicious)
+        df['is_malicious'].iloc[i] = is_malicious[0]
+    return(df)
 
 
-predict("bc1qcj9ujyvrf94wu0902g2lnklzlyn5j5nrr44hwp")
+prediction = predict("bc1qcj9ujyvrf94wu0902g2lnklzlyn5j5nrr44hwp")
+print(prediction)

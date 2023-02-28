71c636cac7d9b7fd92052faa337e7c667f101f29
Shreyans <s.mulkutkar@somaiya.edu>
2023-02-28 11:55:07 +0530

    Added Output 0,1,2.csv

--- Algorithm/DataGenerator.py
+++ Algorithm/DataGenerator.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
     addresses = dataframe['# address'].tolist()
     split_data = list(split(addresses, 10))
 
-    section = 2  # Change this to the section you want to generate
+    section = 0 # Change this to the section you want to generate
     offset = 0  # chnage this to start from the nth row of the chosen section
 
     print(

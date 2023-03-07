82dd72113676ff14a05aacb2e67ea33efe4f88aa
Shreyans <s.mulkutkar@somaiya.edu>
2023-03-07 16:55:29 +0530

    Added Missing Dataset. Created Merged Datasets for Black and White Addresses

--- Algorithm/Merge.py
+++ Algorithm/Merge.py
@@ -4,10 +4,10 @@ import pandas as pd
 def Merge(start, end, filename):
     merged_df = pd.DataFrame()
     for i in range(start, end):
-        df = pd.read_csv("Datasets_Generated/AddressInfo/Output_" + str(i) + ".csv")
+        df = pd.read_csv("Datasets_Generated/WhiteAddressInfo/Output_" + str(i) + ".csv")
         merged_df = merged_df.append(df, ignore_index=True)
     merged_df.to_csv('./Datasets_Generated/Merged_Datasets/' + str(filename) + '.csv', index=False, header=True)
 
 if __name__ == '__main__':
-    Merge(0, 2, "Output")
+    Merge(0, 9, "White_Merged")
     print("Datasets Merged")

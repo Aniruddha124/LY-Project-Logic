3ea09b4fecd1dac7cdb1d8985aee4a92eee57095
Shreyans <s.mulkutkar@somaiya.edu>
2023-03-07 17:17:18 +0530

    Merged White_Merged and Black_Merged into Final_Merged

--- Algorithm/Merge.py
+++ Algorithm/Merge.py
@@ -9,5 +9,11 @@ def Merge(start, end, filename):
     merged_df.to_csv('./Datasets_Generated/Merged_Datasets/' + str(filename) + '.csv', index=False, header=True)
 
 if __name__ == '__main__':
-    Merge(0, 9, "White_Merged")
-    print("Datasets Merged")
+    # Merge(0, 9, "White_Merged")
+    # print("Datasets Merged")
+    df_white = pd.read_csv("./Datasets_Generated/Merged_Datasets/White_Merged.csv")
+    df_black = pd.read_csv("./Datasets_Generated/Merged_Datasets/Black_Merged.csv")
+    df_white['is_Malicious'] = 0
+    df_black['is_Malicious'] = 1
+    df = df_white.append(df_black, ignore_index=True)
+    df.to_csv('./Datasets_Generated/Merged_Datasets/Final_Merged.csv', index=False, header=True)
